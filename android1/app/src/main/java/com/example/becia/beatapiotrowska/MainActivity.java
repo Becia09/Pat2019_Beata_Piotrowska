c5038ab0ead9ac611fb9f825dac3a0fbb62562aa
Beata <becia09269@wp.pl>
2019-01-04 17:17:42 +0100

    function click and postDelayed

--- android1/app/src/main/java/com/example/becia/beatapiotrowska/MainActivity.java
+++ android1/app/src/main/java/com/example/becia/beatapiotrowska/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.becia.beatapiotrowska;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Runnable runnable;
+    public Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+        //Runnable r;
+        this.handler = new android.os.Handler();
+        this.handler.postDelayed(
+                this.runnable = new Runnable() {
+                    public void run() {
+                        Log.i("tag", "This'll run 5000 milliseconds later");
+                        Intent intent2;
+                        intent2 = new Intent(MainActivity.this, MainScreen.class);
+                        startActivity(intent2);
+                    }
+                },
+                5000);
+        //this.handler.removeCallbacks(this.runnable);
     }
 
     @Override
@@ -39,5 +60,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void click(View view) {
+        this.handler.removeCallbacks(this.runnable);
+        /*Intent intent;
+        intent = new Intent(MainActivity.this, MainScreen.class);
+        startActivity(intent);*/
     }
 }
