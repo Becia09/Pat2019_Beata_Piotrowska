3eb85c2bbd2e9a6efcdaa5b00626498bb12267b3
Beata <becia09269@wp.pl>
2019-01-15 15:08:03 +0100

    editText

--- android1/app/src/main/java/com/example/becia/beatapiotrowska/MainScreen.java
+++ android1/app/src/main/java/com/example/becia/beatapiotrowska/MainScreen.java
@@ -1,11 +1,14 @@
 package com.example.becia.beatapiotrowska;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -15,6 +18,21 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        EditText textEmail = (EditText) findViewById(R.id.email);
+        EditText textPassword = (EditText) findViewById(R.id.password);
+
+
+        Button buttonLogin = (Button)findViewById(R.id.login);  //przycisk logowania
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent3;
+                intent3 = new Intent(MainScreen.this, MainActivity.class);
+                startActivity(intent3);
+            }
+        });
+
     }
 
 }
