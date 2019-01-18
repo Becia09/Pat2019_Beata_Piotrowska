073fbd978bd6d6afb1179c54f9fbfc6f663c50b7
Beata <becia09269@wp.pl>
2019-01-19 00:26:22 +0100

    refaktoring

--- android1/app/src/main/java/com/example/becia/beatapiotrowska/MainActivity.java
+++ android1/app/src/main/java/com/example/becia/beatapiotrowska/MainActivity.java
@@ -3,14 +3,10 @@ package com.example.becia.beatapiotrowska;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,30 +16,18 @@ public class MainActivity extends AppCompatActivity {
 
     public FileSession fs;
 
-    /*MainActivity()
-    {
-        super();
-        /*try{
-            fs = new FileSession();
-        }
-        catch (ExceptionInInitializerError e) {
-            fs = FileSession.getInstance();
-        }
-    }*/
-
     public void delayActivity() {
         if (false == flag){
             this.handler = new android.os.Handler();
-            this.handler.postDelayed(		//po 5 sekundach zmienia ekran/aktywność
+            this.handler.postDelayed(
                     this.runnable = new Runnable() {
                         public void run() {
-                            //Log.i("tag", "This'll run 5000 milliseconds later");
                             Intent intent2;
                             intent2 = new Intent(MainActivity.this, MainScreen.class);
                             startActivity(intent2);
                         }
                     },
-                    1000);
+                    3000);
             flag = true;
         }
     }
@@ -64,32 +48,12 @@ public class MainActivity extends AppCompatActivity {
             fs = FileSession.getInstance();
         }
 
-
         delayActivity();
     }
 
 
-    /*public void click(View view) {
-        this.handler.removeCallbacks(this.runnable); //anuluje działanie funcji postDelayed
-    }*/
-
     @Override
     public void onBackPressed() {
-        this.handler.removeCallbacks(this.runnable); //anuluje działanie funcji postDelayed z użyciem systemowego przycisku back
-
-        if (flag) {
-            super.onBackPressed();
-
-            Intent intentLogIn;
-            intentLogIn = new Intent(MainActivity.this, LogedScreen.class);
-            startActivity(intentLogIn);
-        }
-    }
-
-
-    @Override
-    protected void onPause(){
-        Log.i("tag", "Funkcja onPause, MAinActivity - SplashScreen");
-        super.onPause();
+        this.handler.removeCallbacks(this.runnable);
     }
 }
--- android1/app/src/main/java/com/example/becia/beatapiotrowska/MainScreen.java
+++ android1/app/src/main/java/com/example/becia/beatapiotrowska/MainScreen.java
@@ -1,12 +1,11 @@
-package com.example.becia.beatapiotrowska; //-------------------------------dodać regexy do stringów
+package com.example.becia.beatapiotrowska;
 
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -17,18 +16,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainScreen extends AppCompatActivity {
 
-    public Pattern compiledPatternEmail;// = Pattern.compile("Marcin");
+    public Pattern compiledPatternEmail;
     public Pattern compiledPatternPassword;
 
-    public Matcher matcherEmail;// = compiledPattern.matcher("Nazywam sie Marcin Pietraszek");
+    public Matcher matcherEmail;
     public Matcher matcherPassword;
 
     EditText textEmail;
@@ -41,42 +37,9 @@ public class MainScreen extends AppCompatActivity {
     public TextView incorrectPassword;
 
 
-    private String path = Environment.getExternalStorageDirectory().toString() + "/"; //"/ifLoged/";
+    private String path = Environment.getExternalStorageDirectory().toString() + "/";
     private final int MEMORY_ACCES = 5;
 
-    //FileSession fs;
-
-    //File file;
-
-    /*public void createDir(){
-        File folder = new File(path);
-        if (!folder.exists()){
-            try{
-                folder.mkdir();
-            }
-            catch (Exception e){
-                Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
-            }
-        }
-    }*/
-
-
-
-    /*public void createFile(){
-        file = new File(path + "/" + /*System.currentTimeMillis() +*//* "loged.txt");
-        FileOutputStream fOut;
-        OutputStreamWriter myOutWriter;
-        try {
-            fOut = new FileOutputStream(file);
-            myOutWriter = new OutputStreamWriter(fOut);
-            myOutWriter.append(strEmail);
-            myOutWriter.close();
-            fOut.close();
-        }
-        catch (Exception e){
-            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
-        }
-    }*/
 
 
     /*public boolean patternCorrect(EditText text, String str, Pattern pattern, Matcher matcher) {
@@ -135,10 +98,10 @@ public class MainScreen extends AppCompatActivity {
 
 
             compiledPatternEmail = Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,6}$");
-            compiledPatternPassword = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$");//("[[a-z]+[A-Z]+[0-9]+]{8,}");
+            compiledPatternPassword = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$");
 
 
-            Button buttonLogin = (Button) findViewById(R.id.login);  //przycisk logowania
+            Button buttonLogin = (Button) findViewById(R.id.login);
             buttonLogin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -152,43 +115,25 @@ public class MainScreen extends AppCompatActivity {
                     matcherEmail = compiledPatternEmail.matcher(strEmail);
                     matcherPassword = compiledPatternPassword.matcher(strPassword);
 
-                    Log.i("tag", "strEmail: " + strEmail);
-                    Log.i("tag", "strPassword: " + strPassword);
-
                     incorrectEmail = (TextView) findViewById(R.id.incorrectEmail);
                     incorrectPassword = (TextView) findViewById(R.id.incorrectPassword);
                     incorrectEmail.setText("");
                     incorrectPassword.setText("");
                     if (false == matcherEmail.matches()){
-                        Log.i("tag", "email niepoprawny");
                         incorrectEmail.setText("Email niepoprawny");
                     }
 
                     if (false == matcherPassword.matches()){
-                        Log.i("tag", "hasło niepoprawne");
                         incorrectPassword.setText("Hasło niepoprawne");
                     }
 
                     if(matcherEmail.matches() && matcherPassword.matches()){
-
-
-                        /*try{
-                            fs = new FileSession();
-                        }
-                        catch (ExceptionInInitializerError e) {
-                            fs = FileSession.getInstance();
-                        }*/
-
 
                     FileSession.getInstance().createFile();
 
                     Intent intentLogin;
                     intentLogin = new Intent(MainScreen.this, LogedScreen.class);
                     startActivity(intentLogin);
-
-
-                    //createDir();
-
                     }
                 }
             });
